dd5577144a2ba8d9cc5a02e1ea6b20b5425b468f
Kyle <kyle29.zero@gmail.com>
2026-08-18 18:21:05 +0800

    Updates

--- _next/static/-uQj_oyr2Kfxi9zucIfRD/_buildManifest.js
+++ _next/static/-uQj_oyr2Kfxi9zucIfRD/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/karaoke-reading-assessment//_next/:path+",
+        "destination": "/karaoke-reading-assessment/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
